8946ce3f38eb78d5e5c0d1cd07c2280112bba28f
ZeeiD <zeeistorage@emailsultan.com>
2020-09-15 23:23:47 +0700

    Users API

--- application/controllers/api/Users.php
+++ application/controllers/api/Users.php
@@ -7,47 +7,123 @@ require APPPATH . 'libraries/Format.php';
 
 class Users extends REST_Controller {
 
-    public function index_get(){
+    public function index_get($id=''){
         
-        $page = $this->input->get('page');
-        if($page==NULL){
-            $page=1;
+
+        if($id==NULL||$id==''){
+            $page = $this->input->get();
+            if($page==NULL){
+                $page=1;
+                $data = $this->curlku($page);
+    
+                if($data){
+                    $this->set_response(json_decode($data), REST_Controller::HTTP_OK);
+                }else{
+                    $this->set_response([
+                        'status' => False,
+                        'data' => [],
+                        'message' => 'Gagal Request'
+                    ], REST_Controller::HTTP_NOT_FOUND);
+                }
+                
+            }elseif ($this->input->get('id')) {
+                 // Panggil Single User
+                $data = $this->single_user($this->input->get('id'));
+                if($data){
+                    $this->set_response(json_decode($data), REST_Controller::HTTP_OK);
+                }else{
+                    $this->set_response([
+                        'status' => False,
+                        'data' => [],
+                        'message' => 'Gagal Request'
+                    ], REST_Controller::HTTP_NOT_FOUND);
+                }
+                    
+            }else{
+                $page=$this->input->get('page');
+                
+                $data = $this->curlku($page);
+    
+                if($data){
+                    $this->set_response(json_decode($data), REST_Controller::HTTP_OK);
+                }else{
+                    $this->set_response([
+                        'status' => False,
+                        'data' => [],
+                        'message' => 'Gagal Request'
+                    ], REST_Controller::HTTP_NOT_FOUND);
+                }
+            }
+            
+            
+        }else{
+            $data = $this->single_user($id);
+            if($data){
+                $this->set_response(json_decode($data), REST_Controller::HTTP_OK);
+            }else{
+                $this->set_response([
+                    'status' => False,
+                    'data' => [],
+                    'message' => 'Gagal Request'
+                ], REST_Controller::HTTP_NOT_FOUND);
+            }
         }
-        $data = $this->curlku($page);
+
         
-        // echo $page;
     }
 
     private function curlku($page){
-    // $now = new DateTime();
-    $now = new DateTime(null, new DateTimeZone('UTC'));
-    // echo $now->format('Y-m-d H:i:s');    // MySQL datetime format
-    // echo "<br>";
-    // echo $now->getTimestamp();  
-    // $timestam = $now->getTimestamp();
+        // $now = new DateTime();
+        $now = new DateTime(null, new DateTimeZone('UTC'));
+        // echo $now->format('Y-m-d H:i:s');    // MySQL datetime format
+        // echo "<br>";
+        // echo $now->getTimestamp();  
+        // $timestam = $now->getTimestamp();
 
-    // die();
-    // persiapkan curl
-    $ch = curl_init(); 
+        // die();
+        // persiapkan curl
+        $ch = curl_init(); 
 
-    // set url 
-    curl_setopt($ch, CURLOPT_URL, "https://reqres.in/api/users?page=$page");
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    //             'X-rs-id: 3372165',
-    //             'X-Timestamp: '.$timestam.'',
-    //             'X-pass: S!rs2020!!',
-    //             ));
-    // return the transfer as a string 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        // set url 
+        curl_setopt($ch, CURLOPT_URL, "https://reqres.in/api/users?page=$page");
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        //             'X-rs-id: 3372165',
+        //             'X-Timestamp: '.$timestam.'',
+        //             'X-pass: S!rs2020!!',
+        //             ));
+        // return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 
-    // $output contains the output string 
-    $output = curl_exec($ch); 
+        // $output contains the output string 
+        $output = curl_exec($ch); 
 
-    // tutup curl 
-    curl_close($ch);      
+        // tutup curl 
+        curl_close($ch);      
 
-    // menampilkan hasil curl
-    return $output;
+        // menampilkan hasil curl
+        return $output;
+    }
+
+    private function single_user($id){
+        $ch = curl_init(); 
+        // set url 
+        curl_setopt($ch, CURLOPT_URL, "https://reqres.in/api/users/$id");
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        //             'X-rs-id: 3372165',
+        //             'X-Timestamp: '.$timestam.'',
+        //             'X-pass: S!rs2020!!',
+        //             ));
+        // return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+        // $output contains the output string 
+        $output = curl_exec($ch); 
+
+        // tutup curl 
+        curl_close($ch);      
+
+        // menampilkan hasil curl
+        return $output;
     }
 
 }
